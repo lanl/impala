3791848d38715d777f055c92c2e6f7827cdf2906
Peter <ptrubey@ucsc.edu>
2020-08-03 13:55:39 -0700

    added copper to the load_data script

--- calibration/py_calibration_hier/load_data.py
+++ calibration/py_calibration_hier/load_data.py
@@ -14,7 +14,7 @@ import random
 # Defining strings for creating and updating tables
 
 # Meta Table Creation String
-meta_table_create = """CREATE TABLE meta (
+meta_create = """CREATE TABLE meta (
     type TEXT,
     temperature REAL,
     edot REAL,
@@ -25,16 +25,16 @@ meta_table_create = """CREATE TABLE meta (
     );
 """
 
+shpb_data_create = """ CREATE TABLE {}(strain REAL, stress REAL); """
+shpb_meta_insert = """ INSERT INTO
+        meta(type, temperature, edot, emax, pname, fname, table_name)
+        values (?,?,?,?,?,?,?);
+        """
+shpb_data_insert = """ INSERT INTO {}(strain, stress) values (?,?); """
+
 def load_Ti64_shpb(curs):
     global ti64_curr_id
     shpb_emax = 0
-
-    data_table_create = """ CREATE TABLE {}(strain REAL, stress REAL); """
-    meta_table_insert = """ INSERT INTO
-            meta(type, temperature, edot, emax, pname, fname, table_name)
-            values (?,?,?,?,?,?,?);
-            """
-    data_table_insert = """ INSERT INTO {}(strain, stress) values (?,?); """
 
     base_path = '../../data/ti-6al-4v/Data/SHPB'
     papers = [os.path.split(f)[1] for f in glob.glob(base_path + '/*')]
@@ -53,7 +53,7 @@ def load_Ti64_shpb(curs):
             fname = os.path.splitext(os.path.basename(file))[0]
             data  = pd.read_csv(file, skiprows = range(0,17)).values
             data[:,1] = data[:,1] * 1e-5 # Transform from MPa to Mbar
-            xps.append({'data' : data[1:,:], 'temp' : temp, 'edot' : edot,
+            xps.append({'data' : data[1:], 'temp' : temp, 'edot' : edot,
                         'pname' : pname, 'fname' : fname})
             shpb_emax = max(shpb_emax, data[:,0].max())
 
@@ -63,16 +63,16 @@ def load_Ti64_shpb(curs):
         ti64_curr_id += 1
         table_name = 'data_{}'.format(ti64_curr_id)
         # Create the relevant data table
-        curs.execute(data_table_create.format(table_name))
+        curs.execute(shpb_data_create.format(table_name))
         # Create the corresponding line in the meta table
         curs.execute(
-            meta_table_insert,
+            shpb_meta_insert,
             ('shpb', xp['temp'], xp['edot'], shpb_emax,
                 xp['pname'], xp['fname'], table_name)
             )
         # Fill the data tables
         curs.executemany(
-            data_table_insert.format(table_name),
+            shpb_data_insert.format(table_name),
             xp['data'].tolist()
             )
     return
@@ -97,7 +97,7 @@ def load_Ti64():
     # Start the SQLite connection
     connection = sql.connect('./data_Ti64.db')
     cursor = connection.cursor()
-    cursor.execute(meta_table_create)
+    cursor.execute(meta_create)
     connection.commit()
     # Load the data
     current_id = 0
@@ -109,7 +109,81 @@ def load_Ti64():
     connection.close()
     return
 
+def load_copper_shpb(curs):
+    global copper_curr_id
+    shpb_emax = 0
+
+    paths_hb = [
+        './copper/CuRT203.txt',
+        './copper/Cu20203.txt',
+        './copper/Cu40203.txt',
+        './copper/Cu60203.txt',
+        './copper/CuRT10-1.SRC.txt',
+        './copper/CuRT10-3.SRC.txt',
+        ]
+    temps_hb = np.array([298., 473., 673., 873., 298., 298.])
+    edots_hb = np.array([2000., 2000., 2000., 2000., 0.1, 0.001])
+    fnames_hb = [os.path.basename(path) for path in paths_hb]
+
+    xps = []
+
+    for i in range(len(paths_hb)):
+        data = (pd.read_csv(paths_hb[i], sep = '\s+').values)
+        data[:,1] = data[:,1] * 1e-5
+        temp = temps_hb[i]
+        edot = edots_hb[i]
+        fname = fnames_hb[i]
+        pname = ''
+        shpb_emax = max(shpb_emax, data[:,0].max())
+        xps.append({'data' : data[1:], 'temp' : temp, 'edot' : edot,
+                    'pname' : pname, 'fname' : fname})
+
+    for xp in xps:
+        copper_curr_id += 1
+        table_name = 'data_{}'.format(copper_curr_id)
+        curs.execute(shpb_data_create.format(table_name))
+        curs.execute(
+            shpb_meta_insert,
+            ('shpb', xp['temp'], xp['edot'], shpb_emax,
+                xp['pname'], xp['fname'], table_name)
+            )
+        curs.executemany(
+            shpb_data_insert.format(table_name),
+            xp['data'].tolist()
+            )
+    return
+
+def load_copper_tc(curs):
+    global copper_curr_id
+    return
+
+def load_copper_fp(curs):
+    global copper_curr_id
+    return
+
+def load_copper():
+    global copper_curr_id
+    copper_curr_id = 0
+    shpb_emax = 0
+
+    if os.path.exists('./data_copper.db'):
+        os.remove('./data_copper.db')
+
+    connection = sql.connect('./data_copper.db')
+    cursor = connection.cursor()
+    cursor.execute(meta_create)
+    connection.commit()
+
+    load_copper_shpb(cursor)
+    load_copper_tc(cursor)
+    load_copper_fp(cursor)
+
+    connection.commit()
+    connection.close()
+    return
+
 if __name__ == '__main__':
-    load_Ti64()
+    # load_Ti64()
+    load_copper()
 
 # EOF
