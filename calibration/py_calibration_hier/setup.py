e813b39d4120ec54b4123bdf96bf37f76ef102de
Peter <ptrubey@ucsc.edu>
2020-08-04 19:01:22 -0700

    defining language_level in setup

--- calibration/py_calibration_hier/setup.py
+++ calibration/py_calibration_hier/setup.py
@@ -3,11 +3,11 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules = cythonize('physical_models_c.pyx', annotate = True),
+    ext_modules = cythonize('physical_models_c.pyx', annotate = True, language_level = 3),
     include_dirs = [numpy.get_include()],
     )
 setup(
-    ext_modules = cythonize('pointcloud.pyx', annotate = True),
+    ext_modules = cythonize('pointcloud.pyx', annotate = True, language_level = 3),
     include_dirs = [numpy.get_include()],
     )
 
