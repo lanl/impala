0a91009c02675cad8e1ad0f183ade32bc95abd9f
Peter <ptrubey@ucsc.edu>
2020-09-10 16:52:47 -0700

    sbatch script for ti64

---
+++ calibration/py_calibration_hier/test_ti64_clst.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -l
+#SBATCH --job-name=impala_ti64
+#SBATCH --output=res_impala_clust_ti64.txt
+#SBATCH --partition=amd-rome
+#SBATCH --time=72:00:00
+#SBATCH --qos=long
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ptrubey@lanl.gov
+
+cd ~/impala/calibration/py_calibration_hier
+echo start
+conda activate impala
+module load openmpi
+mpiexec -np 16 python -m mpi4py test_ti64_clst.py
+echo end
