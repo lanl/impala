9c1821d315f7b4b6a56a87652f27faaadf754313
Peter Randolph Trubey - 343675 <ptrubey@darwin-fe0.lanl.gov>
2020-09-15 12:31:23 -0600

    testing scripts

--- calibration/py_calibration_hier/test_ti64_clst.sh
+++ calibration/py_calibration_hier/test_ti64_clst.sh
@@ -1,8 +1,8 @@
-#!/bin/sh -l
+#!/bin/bash -l
 #SBATCH --job-name=impala_ti64
 #SBATCH --output=res_impala_clust_ti64.txt
-#SBATCH --partition=amd-rome
-#SBATCH --time=72:00:00
+#SBATCH --partition=amd-rome,amd-rome-gpu,amd-epyc,amd-epyc-gpu
+#SBATCH --time=48:00:00
 #SBATCH --qos=long
 #SBATCH --nodes=1
 #SBATCH --mail-type=ALL
@@ -10,6 +10,7 @@
 
 cd ~/impala/calibration/py_calibration_hier
 echo start
+conda init bash
 conda activate impala
 module load openmpi
 mpiexec -np 16 python -m mpi4py test_ti64_clst.py
