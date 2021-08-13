bf262ddf624f4f981a34d0ec095fe8f35b566ca4
Peter <ptrubey@ucsc.edu>
2021-08-12 20:18:32 -0700

    hierarchical ptw maybe

--- calibration/superCal/models_test.py
+++ calibration/superCal/models_test.py
@@ -1,20 +1,33 @@
 import numpy as np
 import pyBASS as pb
 import physical_models_vec as pm_vec
+from itertools import cycle
 from scipy.interpolate import interp1d
+
+epsilon = 1e-5
 
 #####################
 # model classes should have eval method and stochastic attribute
 #####################
 class ModelBassPca:
-    def __init__(self, bmod, input_names, pool=True): # input_names is a list of the names of the inputs to bmod
+    """ PCA Based Model Emulator """
+    def __init__(self, bmod, input_names, pool=True):
+        """
+        bmod        : ~
+        input_names : list of the names of the inputs to bmod
+        pool        : Whether using the pooled model
+        """
         self.mod = bmod
         self.stochastic = True
         self.nmcmc = len(bmod.bm_list[0].samples.s2)
         self.input_names = input_names
         self.pool = pool # I don't do anything with this below...in hierarchical case, requires one theta for each BassPCA model
+        return
 
     def eval(self, parmat):
+        """
+        parmat : ~
+        """
         parmat_array = np.vstack([parmat[v] for v in self.input_names]).T # get correct subset/ordering of inputs
         ind = 0#np.random.choice(range(self.nmcmc), 1)
         return self.mod.predict(parmat_array, mcmc_use=np.array([ind]), nugget=True)[0, :, :]
@@ -29,13 +42,21 @@ class ModelF:
         return np.apply_along_axis(self.mod, 1, parmat_array)
 
 class ModelPTW:
-    def __init__(self, temps, edots, consts, strain_histories, pool=True): # list of strain_histories, eval interpolates to these
+    """ PTW Model for Hoppy-Bar / Quasistatic Experiments """
+    def __init__(self, temps, edots, consts, strain_histories, pool=True):
+        """
+        temps  : ~
+        edots  : ~
+        consts : dictionary of constants for PTW model
+        strain_histories : List of strain histories for HB/Quasistatic Experiments
+        """
         self.meas_strain_histories = strain_histories
         self.meas_strain_max = np.array([v.max() for v in strain_histories])
         self.strain_max = self.meas_strain_max.max()
         self.nhists = sum([len(v) for v in strain_histories])
-        #self.sim_strain_histories = pm_vec.generate_strain_history(self.strain_max, edots, Nhist=100)
-        self.model = pm_vec.MaterialModel(flow_stress_model=pm_vec.PTW_Yield_Stress, shear_modulus_model=pm_vec.Stein_Shear_Modulus)
+        self.model = pm_vec.MaterialModel(
+            flow_stress_model=pm_vec.PTW_Yield_Stress, shear_modulus_model=pm_vec.Stein_Shear_Modulus,
+            )
         self.constants = consts
         self.temps = temps
         self.edots = edots
@@ -44,91 +65,46 @@ class ModelPTW:
         self.Nhist = 50
         self.stochastic = False
         self.pool = pool
+        return
 
-        self.xpred = np.hstack(strain_histories) # flatten for use with vectorized interpolator, strains for 1st experiment, then strains for 2nd experiment, etc.
-        flatten_shiftx = []
-        for i in range(self.nexp):
-            flatten_shiftx.append(np.ones(len(self.meas_strain_histories[i]))*(self.strain_max+.0001)*i)
-        self.flatten_shiftx = np.hstack(flatten_shiftx)
 
-    def eval(self, parmat, expList = None): # note: extra parameters ignored
-        """
-        parmat:  dictionary of parameters
-        explist: array of experiment indices that are under evaluation (dtype = int)
-                if hierarchical, then len(parmat[n][key]) == len(explist)
-        """
-        if expList is None: # Pooled Case
-            nrep = parmat['p'].shape[0]
-            edots = np.kron(np.ones(nrep), self.edots)
-            temps = np.kron(np.ones(nrep), self.temps)
+    def eval(self, parmat): # note: extra parameters ignored
+        """ parmat:  dictionary of parameters """
+        if self.pool:  # Pooled Case
+            nrep = parmat['p'].shape[0] # number of temper temps
             parmat_big = {key : np.kron(np.ones(self.nexp), parm) for key, parm in parmat.items()}
-            strain_maxs = np.kron(np.ones(nrep), self.meas_strain_max)
-            strain_hists = np.kron(np.ones(nrep), self.meas_strain_histories)
-            # xpred_big = np.kron(np.ones(nrep), self.xpred) this isn't gonna work
-        else:                    # hierarchical case
-            edots = self.edots[explist]
-            temps = self.temps[explist]
+        else:          # hierarchical case
+            nrep = parmat['p'].shape[0] // self.nexp # number of temper temps
             parmat_big = parmat
-            strain_maxs = self.meas_strain_max[explist]
-            strain_hists = self.meas_strain_histories[explist]
 
-        # Building xpred vector
-        strain_ends = np.hstack(np.array((0.,)), np.cumsum(strain_maxs + 1e-4)[:-1])
-        flattened_strain = np.hstack([x + y for x, y in zip(strain_hists, strain_ends)])
-        ntot = edots.shape[0]
+        edots = np.kron(np.ones(nrep), self.edots) # 1d vector, nexp * temper_temps
+        temps = np.kron(np.ones(nrep), self.temps) # 1d vector, nexp * temper_temps
+        strain_maxs = np.kron(np.ones(nrep), self.meas_strain_max) # 1d vector, nexp * temper_temps
+        ntot = edots.shape[0]  # nexp * temper_temps
         sim_strain_histories = pm_vec.generate_strain_history_new(strain_maxs, edots, self.Nhist)
         self.model.initialize(parmat_big, self.constants)
         self.model.initialize_state(T = temps, stress = np.zeros(ntot), strain = np.zeros(ntot))
-        state_hists = self.model.compute_state_history(sim_strain_histories)
-        sim_strains = results[:,1]
-        sim_stresses = results[:,2]
+        sim_state_histories = self.model.compute_state_history(sim_strain_histories)
+        sim_strains = sim_state_histories[:,1].T  # 2d array: ntot, Nhist
+        sim_stresses = sim_state_histories[:,2].T # 2d array: ntot, Nhist
 
-        flattened_sim_strain = np.hstack([x + y for x, y in zip(sim_strains, strain_ends])])
+        # Expand/Flatten Simulated strains to single vector--ensure no overlap.
+        strain_ends = np.hstack((0., np.cumsum(strain_maxs + epsilon)[:-1])) # 1d vector, ntot
+        flattened_sim_strain = np.hstack( # 1d vector: ntot * Nhist
+            [x + y for x, y in zip(sim_strains, strain_ends)]
+            )
+        # Expand/flatten simulated stress to single vector
         flattened_sim_stress = np.hstack(sim_stresses)
-
-        ifunc = interp1d(flattened_sim_strain, flattened_sim_stress, kind = linear,
-                                            assume_sorted = True, fill_value = 'extrapolate')
-        ypred = ifunc(flattened_strain).reshape((nrep, self.nhists)) # this can't be right...
-
-        # if self.pool:
-        #     nrep = len(parmat['p'])
-        #     parmat_big = {k: np.kron(v, np.ones((self.nexp))) for k, v in parmat.items()} # parameter values constant while edot/temp loop
-        # else:
-        #     nrep = len(parmat[0]['p']) # hierarchical case should pass a list of dicts
-        #     parmat_big = parmat[0]
-        #     for i in range(1,self.nexp):
-        #         for key in parmat[i]:
-        #             parmat_big[key] = [parmat_big[key], np.kron(parmat[i].items(), np.ones((self.nexp)))]
-
-        # ntot = nrep * self.nexp
-        # edots_big = np.kron(np.ones(nrep), self.edots)
-        # temps_big = np.kron(np.ones(nrep), self.temps)
-        # xpred2 = np.kron(np.ones(nrep), self.xpred)
-        #
-        # flatten_shiftx = []
-        # for i in range(nrep):
-        #     flatten_shiftx.append(self.flatten_shiftx+i*(self.strain_max+.0001)*self.nexp)
-        #
-        # flatten_shiftx = np.hstack(flatten_shiftx)
-        # xpred2 = xpred2 + flatten_shiftx
-        #
-        # sim_strain_histories = pm_vec.generate_strain_history(self.strain_max, edots_big, Nhist=self.Nhist)
-        # # note: everything above here could go in __init__ if we knew nrep -> maybe make a method to change nrep
-        #
-        # self.model.initialize(parmat_big, self.constants)
-        # self.model.initialize_state(T=temps_big, stress=np.repeat(0., ntot), strain=np.repeat(0., ntot))
-        # state_hists = self.model.compute_state_history(sim_strain_histories)
-        #
-        # flatten_shiftx_sim = np.array([float(i) * (self.strain_max+.0001) for i in list(range(ntot))])
-        # strain_hists2 = state_hists[:, 1, :] + np.kron(flatten_shiftx_sim,np.ones((self.Nhist,1)))
-        #
-        # x = strain_hists2.flatten("F") # ordered as 100 strains for first experiment, first param setting, then 100 strains for 2nd experiment, 1st param setting...
-        # y = state_hists[:, 2, :].flatten("F")
-        # ifunc = interp1d(x, y, kind='linear', assume_sorted=False, fill_value="extrapolate")
-        # ypred = ifunc(xpred2).reshape([nrep,self.nhists])
-        # flattening and then interpolating is much faster than looping over interpolating
-
-        return ypred #[x,y,xpred2,state_hists,ypred]
+        # Expand/flatten measured strain to single vector, for each parameter.  Use same
+        #  Computed strain ends to ensure no overlap
+        flattened_strain = np.hstack( # cycle will repeat through measured strain histories
+            [x + y for x, y in zip(cycle(self.meas_strain_histories), strain_ends)]
+            )
+        ifunc = interp1d(  # Generate the interpolation function.
+            flattened_sim_strain, flattened_sim_stress, kind = 'linear', assume_sorted = True
+            )
+        ypred = ifunc(flattened_strain).reshape(nrep, -1)  # Interpolate, and output.
+        return ypred
 
 def interpolate_experiment(args):
     """ Interpolate and predict at x.  Args is tuple(x_observed, y_observed, x_new) """
