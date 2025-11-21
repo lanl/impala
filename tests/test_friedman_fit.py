21af5b5ba0d86d449d5700aed23321398071baec
Arthur Lui <alui@lanl.gov>
2025-11-20 22:14:19 -0700

    update test

--- tests/test_friedman_fit.py
+++ tests/test_friedman_fit.py
@@ -1,33 +1,34 @@
+import numpy as np
+from numpy import ndarray
 from scipy.stats import kstest
 from scipy.stats import uniform as Uniform
-import numpy as np
+
 from impala import superCal as sc
 
-# FIXME: Supported for python>=3.8. This is the type for theta and grid in
-# Friedman. Doesn't work for py3.6.
-#
-# from numpy.typing import ArrayLike
 
 class Friedman:
-    def __init__(self, grid):
+    def __init__(self, grid: ndarray) -> None:
         """
         grid: Grid at which Friedman function is evaluated.
         """
         self.grid = grid
 
-    def __call__(self, theta):
+    def __call__(self, theta: ndarray) -> ndarray:
         """
         theta: Friedman function parameters.
         """
         return (
-            10 * np.sin(np.pi * self.grid * theta[0]) +
-            20 * (theta[1] - .5) ** 2 +
-            10 * theta[2] +
-            5 * theta[3]
+            10 * np.sin(np.pi * self.grid * theta[0])
+            + 20 * (theta[1] - 0.5) ** 2
+            + 10 * theta[2]
+            + 5 * theta[3]
         )
 
+
 def generate_data(n_features: int, gridsize: int):
-    assert n_features >= 4, "Friedman function has 4 parameters, so n_feature must be at least 4."
+    assert n_features >= 4, (
+        "Friedman function has 4 parameters, so n_feature must be at least 4."
+    )
 
     grid = np.linspace(0, 1, gridsize)
     friedman = Friedman(grid)
@@ -38,26 +39,26 @@ def generate_data(n_features: int, gridsize: int):
     theta = np.random.rand(1, n_features)
 
     # True observation error standard deviation.
-    sigma= 0.1
+    sigma = 0.1
 
     mu = np.apply_along_axis(friedman, 1, theta).squeeze()
     yobs = np.random.normal(mu, sigma)
-    param_truth = dict(theta = theta, sigma = sigma)
+    param_truth = dict(theta=theta, sigma=sigma)
 
     return yobs, friedman, n_features, param_truth
 
+
 def test_friedman_fit():
     np.random.seed(0)
-    yobs, friedman, n_features, param_truth = generate_data(n_features=6, gridsize=50)
+    yobs, friedman, n_features, param_truth = generate_data(
+        n_features=6, gridsize=50
+    )
 
-    # Create bounds for each theta. Names (keys) need to be strings. 
-    bounds = {
-        str(index): np.array([0, 1])
-        for index in range(n_features)
-    }
+    # Create bounds for each theta. Names (keys) need to be strings.
+    bounds = {str(index): np.array([0, 1]) for index in range(n_features)}
 
     # Initialize with the only constraints being the bounds.
-    setup = sc.CalibSetup(bounds, constraint_func='bounds')
+    setup = sc.CalibSetup(bounds, constraint_func="bounds")
 
     # Put the Friedman function into the right structure; this could be replaced
     # with an emulator.
@@ -70,19 +71,19 @@ def test_friedman_fit():
         yobs=yobs,
         # model that predicts a vector
         model=model,
-        # yobs error standard deviation estimate (possibly a vector of estimates for
-        # different parts of yobs vector).
-        sd_est=[1.],
-        # yobs error degrees of freedom (larger means more confidence in sd_est),
-        # same shape as sd_est.
+        # yobs error standard deviation estimate (possibly a vector of estimates
+        # for different parts of yobs vector).
+        sd_est=[1.0],
+        # yobs error degrees of freedom (larger means more confidence in
+        # sd_est), same shape as sd_est.
         s2_df=[0],
-        # if sd_est is a vector of length 3, this is a vector of length len(yobs)
-        # with values (0, 1, 2) indicating which sd_est corresponds to which part of
-        # yobs.
-        s2_ind=[0] * len(yobs)
+        # if sd_est is a vector of length 3, this is a vector of length
+        # len(yobs) with values (0, 1, 2) indicating which sd_est corresponds to
+        # which part of yobs.
+        s2_ind=[0] * len(yobs),
     )
 
-    # temperature ladder, typically (1 + step) ** np.arange(ntemps) 
+    # temperature ladder, typically (1 + step) ** np.arange(ntemps)
     setup.setTemperatureLadder(1.05 ** np.arange(40))
 
     # MCMC number of iterations, and how often to take a decorrelation step.
@@ -104,16 +105,20 @@ def test_friedman_fit():
     # uniform.
     superfluous_theta_posterior = theta_posterior[:, num_utilized_parameters:]
     for i, theta in enumerate(superfluous_theta_posterior.T):
-        assert kstest(theta, Uniform().cdf).pvalue > 0.10, f"theta_{i} is not Uniform!"
+        assert kstest(theta, Uniform().cdf).pvalue > 0.10, (
+            f"theta_{i} is not Uniform!"
+        )
 
-    # Test that the posterior for the utilized parameters in theta are
-    # not uniform.
+    # Test that the posterior for the utilized parameters in theta are not
+    # uniform.
     utilized_theta_posterior = theta_posterior[:, :num_utilized_parameters]
     for i, theta in enumerate(utilized_theta_posterior.T):
-        assert kstest(theta, Uniform().cdf).pvalue < 0.01, f"theta_{i} is Uniform!"
+        assert kstest(theta, Uniform().cdf).pvalue < 0.01, (
+            f"theta_{i} is Uniform!"
+        )
 
     # Test that the true values of theta are in the 95% credible interval.
-    lower = np.quantile(theta_posterior, .025, 0)
-    upper = np.quantile(theta_posterior, .975, 0)
-    assert np.all(lower < param_truth['theta'])
-    assert np.all(param_truth['theta'] < upper)
+    lower = np.quantile(theta_posterior, 0.025, 0)
+    upper = np.quantile(theta_posterior, 0.975, 0)
+    assert np.all(lower < param_truth["theta"])
+    assert np.all(param_truth["theta"] < upper)
