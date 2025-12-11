e22f67d9f5d0a33a0f8b6b94fa5e0bcd8c77f6be
Arthur Lui <alui@lanl.gov>
2025-12-11 16:57:15 -0700

    format

--- src/impala/physics/physical_models_vec.py
+++ src/impala/physics/physical_models_vec.py
@@ -74,7 +74,7 @@ class BaseModel(object):
         self.parent = parent
 
 
-########################    
+########################
 # Specific Heat Models
 ########################
 
@@ -134,17 +134,24 @@ class Quadratic_Specific_Heat(BaseModel):
         )
         return cnow
 
+
 class Cubic_Specific_Heat(BaseModel):
     """
     Cubic Specific Heat Model
     """
-    consts = ['c0', 'c1', 'c2','c3']
+
+    consts = ["c0", "c1", "c2", "c3"]
 
     def value(self, *args):
-
-        tnow=self.parent.state.T
-        cnow=self.parent.parameters.c0+self.parent.parameters.c1*tnow+self.parent.parameters.c2*tnow**2+self.parent.parameters.c3*tnow**3
+        tnow = self.parent.state.T
+        cnow = (
+            self.parent.parameters.c0
+            + self.parent.parameters.c1 * tnow
+            + self.parent.parameters.c2 * tnow**2
+            + self.parent.parameters.c3 * tnow**3
+        )
         return cnow
+
 
 class Piecewise_Linear_Specific_Heat(BaseModel):
     """
@@ -197,30 +204,57 @@ class Piecewise_Quadratic_Specific_Heat(BaseModel):
         cnow = pow_0_coeff + pow_1_coeff * tnow + pow_2_coeff * tnow * tnow
         return cnow
 
+
 class Piecewise_Cubic_Specific_Heat(BaseModel):
     """
     Cubic Specific Heat Model
     Piecewise Quadratic Specific Heat Model
     Cv (T) = c0_0 + c1_0 * T + c2_0 * T**2  + c3_0 * T**3 for T<=T_t
-    Cv (T) = c0_1 + c1_1 * T + c2_1 * T**2  + c3_1 * T**3 for T>T_t          
-   """
-    consts = ['T_t','c0_0', 'c1_0', 'c2_0', 'c3_0', 'c0_1', 'c1_1', 'c2_1', 'c3_1']
+    Cv (T) = c0_1 + c1_1 * T + c2_1 * T**2  + c3_1 * T**3 for T>T_t
+    """
+
+    consts = [
+        "T_t",
+        "c0_0",
+        "c1_0",
+        "c2_0",
+        "c3_0",
+        "c0_1",
+        "c1_1",
+        "c2_1",
+        "c3_1",
+    ]
 
     def value(self, *args):
-        tnow=self.parent.state.T
-        pow_0_coeff = np.repeat(self.parent.parameters.c0_0,len(tnow))
-        pow_1_coeff = np.repeat(self.parent.parameters.c1_0,len(tnow))
-        pow_2_coeff = np.repeat(self.parent.parameters.c2_0,len(tnow))
-        pow_3_coeff = np.repeat(self.parent.parameters.c3_0,len(tnow))
-        
-        pow_0_coeff[np.where(tnow > self.parent.parameters.T_t)] =  self.parent.parameters.c0_1
-        pow_1_coeff[np.where(tnow > self.parent.parameters.T_t)] =  self.parent.parameters.c1_1
-        pow_2_coeff[np.where(tnow > self.parent.parameters.T_t)] =  self.parent.parameters.c2_1
-        pow_3_coeff[np.where(tnow > self.parent.parameters.T_t)] =  self.parent.parameters.c3_1
-        
-        cnow = pow_0_coeff + pow_1_coeff * tnow + pow_2_coeff * tnow * tnow + pow_3_coeff * tnow * tnow* tnow
+        tnow = self.parent.state.T
+        pow_0_coeff = np.repeat(self.parent.parameters.c0_0, len(tnow))
+        pow_1_coeff = np.repeat(self.parent.parameters.c1_0, len(tnow))
+        pow_2_coeff = np.repeat(self.parent.parameters.c2_0, len(tnow))
+        pow_3_coeff = np.repeat(self.parent.parameters.c3_0, len(tnow))
+
+        pow_0_coeff[np.where(tnow > self.parent.parameters.T_t)] = (
+            self.parent.parameters.c0_1
+        )
+        pow_1_coeff[np.where(tnow > self.parent.parameters.T_t)] = (
+            self.parent.parameters.c1_1
+        )
+        pow_2_coeff[np.where(tnow > self.parent.parameters.T_t)] = (
+            self.parent.parameters.c2_1
+        )
+        pow_3_coeff[np.where(tnow > self.parent.parameters.T_t)] = (
+            self.parent.parameters.c3_1
+        )
+
+        cnow = (
+            pow_0_coeff
+            + pow_1_coeff * tnow
+            + pow_2_coeff * tnow * tnow
+            + pow_3_coeff * tnow * tnow * tnow
+        )
         return cnow
-########################    
+
+
+########################
 # Density Models
 ########################
 
@@ -367,11 +401,18 @@ class Cubic_Melt_Temperature(BaseModel):
     """
     Cubic Melt Temperature Model
     """
-    consts=['tm0', 'tm1', 'tm2', 'tm3']
+
+    consts = ["tm0", "tm1", "tm2", "tm3"]
+
     def value(self, *args):
-        rnow=self.parent.state.rho
-        tmeltnow=self.parent.parameters.tm0+self.parent.parameters.tm1*rnow+self.parent.parameters.tm2*rnow**2+self.parent.parameters.tm3*rnow**3
-        #print(tmeltnow)
+        rnow = self.parent.state.rho
+        tmeltnow = (
+            self.parent.parameters.tm0
+            + self.parent.parameters.tm1 * rnow
+            + self.parent.parameters.tm2 * rnow**2
+            + self.parent.parameters.tm3 * rnow**3
+        )
+        # print(tmeltnow)
         return tmeltnow
 
 
