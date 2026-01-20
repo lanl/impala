a46ba66073657b61c6def1c812fa19474a1d019b
Arthur Lui <alui@lanl.gov>
2026-01-20 15:13:20 -0700

    moved numba from group -> optional dep

--- tests/test_physical_models_vec.py
+++ tests/test_physical_models_vec.py
@@ -8,7 +8,7 @@ from impala import superCal as sc
 
 
 def test_physics():
-    '''Regression test for PTW'''
+    """Regression test for PTW"""
     test_dir = Path(__file__).parent
     data_dir = test_dir / "data"
 
@@ -68,11 +68,11 @@ def test_physics():
 
 
 def test_constparams():
-    '''tests the pooled calibration of the ptw model some of its parameters kept
-       constant for artificial data. Despite the small number of MCMC iterations,
-       we check that the "best" parameters are within 1 permille of the values
-       used to generate the artifical data, as well as that the keys of "params"
-       match the keys of the calibrated parameters.'''
+    """tests the pooled calibration of the ptw model some of its parameters kept
+    constant for artificial data. Despite the small number of MCMC iterations,
+    we check that the "best" parameters are within 1 permille of the values
+    used to generate the artifical data, as well as that the keys of "params"
+    match the keys of the calibrated parameters."""
     test_dir = Path(__file__).parent
     data_dir = test_dir / "data"
 
@@ -100,13 +100,12 @@ def test_constparams():
         "y0": np.array([0.01]),
         "yInf": np.array([0.003]),
     }
-    
+
     bounds_ptw = {}
-    for k,v in params.items():
-        bounds_ptw[k] = (0.98*v[0],1.02*v[0])
-        if v<0:
-            bounds_ptw[k] = (1.02*v[0],0.98*v[0])
-    
+    for k, v in params.items():
+        bounds_ptw[k] = (0.98 * v[0], 1.02 * v[0])
+        if v < 0:
+            bounds_ptw[k] = (1.02 * v[0], 0.98 * v[0])
 
     ptw = physics.MaterialModel(
         flow_stress_model=physics.PTW_Yield_Stress,
@@ -136,31 +135,36 @@ def test_constparams():
 
     # Test that the current model output matches the baseline, this time using more constants
     assert np.allclose(strainstress_old, strainstress_new)
-    
+
     setup_pool_ptw = sc.CalibSetup(bounds_ptw, sc.constraints_ptw)
-    model_pool_ptw = sc.ModelMaterialStrength(temps=np.array(temp), 
-            edots=np.array(edot), 
-            consts=consts, 
-            strain_histories=[sim_strains],
-            flow_stress_model='PTW_Yield_Stress',
-            melt_model= 'Constant_Melt_Temperature',
-            shear_model= 'Stein_Shear_Modulus',
-            specific_heat_model='Constant_Specific_Heat',
-            density_model='Constant_Density',
-            pool=True, s2='gibbs')
-    yobs = sim_stresses[:,0]
-    setup_pool_ptw.addVecExperiments(yobs=yobs,
-            model=model_pool_ptw,
-            sd_est=[1.0],
-            s2_df=[0],
-            s2_ind=[0] * len(yobs))
-    setup_pool_ptw.setTemperatureLadder(1.05**np.arange(20)) 
+    model_pool_ptw = sc.ModelMaterialStrength(
+        temps=np.array(temp),
+        edots=np.array(edot),
+        consts=consts,
+        strain_histories=[sim_strains],
+        flow_stress_model="PTW_Yield_Stress",
+        melt_model="Constant_Melt_Temperature",
+        shear_model="Stein_Shear_Modulus",
+        specific_heat_model="Constant_Specific_Heat",
+        density_model="Constant_Density",
+        pool=True,
+        s2="gibbs",
+    )
+    yobs = sim_stresses[:, 0]
+    setup_pool_ptw.addVecExperiments(
+        yobs=yobs,
+        model=model_pool_ptw,
+        sd_est=[1.0],
+        s2_df=[0],
+        s2_ind=[0] * len(yobs),
+    )
+    setup_pool_ptw.setTemperatureLadder(1.05 ** np.arange(20))
     setup_pool_ptw.setMCMC(nmcmc=2000, decor=100)
-    np.seterr(divide = 'ignore')
-    np.seterr(invalid = 'ignore')
+    np.seterr(divide="ignore")
+    np.seterr(invalid="ignore")
     out = sc.calibPool(setup_pool_ptw)
-    for k,v in out.theta_native.items():
-        bestval = np.min(np.abs(v/params[k]-1))
+    for k, v in out.theta_native.items():
+        bestval = np.min(np.abs(v / params[k] - 1))
         # print(k,params[k],bestval)
-        assert bestval<1e-3
-    assert sorted(list(out.theta_native.keys()))==sorted(list(params.keys()))
+        assert bestval < 1e-3
+    assert sorted(list(out.theta_native.keys())) == sorted(list(params.keys()))
