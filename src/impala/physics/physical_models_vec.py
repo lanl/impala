99228e2b24ab35a93cf590d8828ffc421ac3c976
Daniel N. Blaschke <40176390+dblaschke-LANL@users.noreply.github.com>
2026-06-11 01:52:51 -0600

    streamline comments

--- src/impala/physics/physical_models_vec.py
+++ src/impala/physics/physical_models_vec.py
@@ -696,7 +696,7 @@ class Stein_Flow_Stress(BaseModel):
 
     def __init__(self, parent):
         BaseModel.__init__(self, parent)
-        # self.params = ["y0", "a", "b", "beta", "n", "ymax"] # TODO: general to Steinberg-Guinan  (and Lund) model where params a,b are perhaps? for strain-rate dependence
+        # self.params = ["y0", "a", "b", "beta", "n", "ymax"] # TODO: generalize, include strain-rate dependence
         ## params a, b are never used, so drop them:
         self.params = ["y0", "beta", "n", "ymax"]
         self.consts = ["G0", "epsi", "chi"]
