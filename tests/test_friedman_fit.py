d886f779b307b48213294594cc39c63aab98e901
Lauren Joann VanDervort <lvandervort@lanl.gov>
2026-08-19 16:12:36 -0600

    fix ruff

--- tests/test_friedman_fit.py
+++ tests/test_friedman_fit.py
@@ -36,8 +36,10 @@ def generate_data(n_features: int, gridsize: int):
     # True calibration parameter value. Note that the Friedman function uses
     # only the first four elements. So the remaining elements should have
     # uniform posteriors.
-    theta = np.random.rand(1, n_features)
-
+    # theta = np.random.rand(1, n_features)
+    theta = np.repeat(
+        0.5, n_features
+    )  # avoid errors due to true theta being up against bounds
     # True observation error standard deviation.
     sigma = 0.1
 
