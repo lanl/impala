70ec5901c043c23f05bb54e304918a219a81c9b1
Daniel N. Blaschke <40176390+dblaschke-LANL@users.noreply.github.com>
2026-08-19 14:54:34 -0600

    fix import error if impala is not installed
    i.e. make it possible to import a local copy of impala
    without the need to install first

--- src/impala/__init__.py
+++ src/impala/__init__.py
@@ -1,5 +1,11 @@
+from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as _get_version
 
 from . import physics, superCal
 
-__version__ = _get_version("impala-calib")
+try:
+    __version__ = _get_version("impala-calib")
+except PackageNotFoundError:
+    __version__ = (
+        "local"  ## use this if impala is not installed,  but imported locally
+    )
