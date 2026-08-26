7f87a906430d01ba38b4205b36c7a53e28eae230
Daniel N. Blaschke <40176390+dblaschke-LANL@users.noreply.github.com>
2026-08-26 14:16:39 -0600

    fix ruff complaints about __init__.py files

--- src/impala/superCal/__init__.py
+++ src/impala/superCal/__init__.py
@@ -2,3 +2,11 @@ from . import post_process
 from .impala_clust import *
 from .impala_noprobit_emu import *
 from .models_withlik import *
+
+__all__ = ["post_process"]
+for modnm in [
+    impala_clust,
+    impala_noprobit_emu,
+    models_withlik,
+]:
+    __all__ += [nm for nm in dir(modnm) if not nm.startswith("_")]
