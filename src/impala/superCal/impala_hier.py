5b49520e2a671d43b9617be7c32c96a31f7e8bd0
Daniel N. Blaschke <40176390+dblaschke-LANL@users.noreply.github.com>
2026-09-15 12:54:32 -0600

    fix hierarchical calib. getting stuck frequently

--- src/impala/superCal/impala_hier.py
+++ src/impala/superCal/impala_hier.py
@@ -669,7 +669,7 @@ def calibHier(setup):
             np.einsum("t,tpq->tpq", ntheta * setup.itl, Sigma0_inv_curr)
             + theta0_prior_prec,
         )
-        tbar *= 0.0
+        tbar = 0.0
         for i in range(setup.nexp):
             tbar += theta[i][m].sum(axis=1)
         tbar /= ntheta
